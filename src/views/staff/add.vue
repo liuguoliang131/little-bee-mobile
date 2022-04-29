b5d7b7c6053f98c99c1be9947ea7d3b4a8ba64c7
liuguoliang <297776539@qq.com>
2022-04-29 18:01:45 +0800

    `

--- src/views/staff/add.vue
+++ src/views/staff/add.vue
@@ -1,24 +1,43 @@
 <!--
  * @Date: 2022-04-29 16:50:15
  * @LastEditors: 刘国亮
- * @LastEditTime: 2022-04-29 16:50:15
+ * @LastEditTime: 2022-04-29 18:01:36
  * @FilePath: \little-bee-mobile\src\views\staff\add.vue
  * @Description: 添加员工
 -->
 <template>
   <div class="staffAdd">
-    123
+    <bread></bread>
+    <div class="views">
+      
+    </div>
   </div>
 </template>
 
 <script>
+import Bread from '@/components/bread/index'
+import {
+  Button,
+  Icon,
+  Tabbar,
+  TabbarItem,
+  Pagination
+} from 'vant'
 export default {
-
+  name: 'StaffAdd',
+  components: {
+    Bread,
+    VanButton:Button,
+    VanIcon:Icon,
+    VanTabbar: Tabbar,
+    VanTabbarItem: TabbarItem,
+    VanPagination: Pagination
+  }
 }
 </script>
 
 <style scoped lang="less">
 .staffAdd {
-  
+
 }
 </style>
